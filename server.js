e15c764dce5c8a4734d22f505e83f1e17bad4ce8
Glitch (arrasicoi-server) <none>
2021-03-15 05:46:28 +0000

    🙌👐 Checkpoint ./server.js:27659364/10

--- server.js
+++ server.js
@@ -4765,7 +4765,7 @@ var maintainloop = (() => {
     // The NPC function
     let makenpcs = (() => {
         // Make base protectors if needed.
-            let f = (loc, team) => { 
+          /*  let f = (loc, team) => { 
                 let o = new Entity(loc);
                     o.define(Class.baseProtector);
                     o.team = -team;
@@ -4773,12 +4773,12 @@ var maintainloop = (() => {
             };
             for (let i=1; i<5; i++) {
                 room['bas' + i].forEach((loc) => { f(loc, i); }); 
-            }
+            }*/
         // Return the spawning function
         let bots = [];
         return () => {
             let census = {
-                crasher: 0,
+                crasher: 1,
                 miniboss: 0,
                 tank: 0,
             };    
