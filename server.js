a15c9ef58e0543bbf5aeb4f7a5166a2ef8be327f
Glitch (arrasicoi-client) <none>
2021-04-13 01:48:27 +0000

    🛰👟 Checkpoint ./server.js:27659364/48

--- server.js
+++ server.js
@@ -4845,12 +4845,12 @@ Class.basic, Class.anni, Class.factory, Class.overdrive, Class.tripletwin, Class
                 if (bots.length < c.BOTS) {
                     let o = new Entity(room.random());
                     o.color = 17;
-                    o.define(RNGbot) 
+                    o.define(Class.bbot) 
                     o.define(RNGclass)
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
                     
-                    o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
+                    o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37])
                    
                     bots.push(o);
                 }
