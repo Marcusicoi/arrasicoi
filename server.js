a2bc4a19b706455d211e03e625641d2bbc18bf8a
Glitch (arrasicoi-client-f) <none>
2021-07-26 10:15:58 +0000

    🐚💨 Checkpoint ./config.json:27659364/209 ./server.js:27659364/52

--- server.js
+++ server.js
@@ -4849,17 +4849,18 @@ Class.basic, Class.anni, Class.factory, Class.overdrive, Class.tripletwin, Class
          
                 if (bots.length < c.BOTS) {
                     let o = new Entity(room.random());
-                //  o.color = 17;
+                    o.color = 12;
                     o.define(Class.bot) 
                     o.define(ran.chooseBotTank());
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes(); 
+                    o.color = 12
                 //  o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]) 
-                    o.team = ran.chooseBotTeam();
+                /*  o.team = ran.chooseBotTeam();
                     if (o.team === -1) {o.color = 10};
                     if (o.team === -2) {o.color = 11};
                     if (o.team === -3) {o.color = 12};
-                    if (o.team === -4) {o.color = 15};
+                    if (o.team === -4) {o.color = 15}; */
                     bots.push(o);
                 }
                   // Remove dead ones
