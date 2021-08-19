10dbdf9016bb1cb9cc0a46d793a4543b9afe3853
Glitch (arrasicoi-client-f) <none>
2021-08-19 15:08:19 +0000

    😀🍄 Checkpoint ./server.js:27704817/2 ./config.json:27704817/30

--- server.js
+++ server.js
@@ -5182,7 +5182,7 @@ var maintainloop = (() => {
     return () => {
         // Do stuff
         makenpcs();      
-       // makefood(); 
+        makefood(); 
         // Regen health and update the grid
         entities.forEach(instance => {
             if (instance.shield.max) {
