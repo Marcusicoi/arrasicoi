86b32919cf00fe83c6ee07c15a76985e71344cb6
Glitch (arrasicoi-client-f) <none>
2021-08-20 14:24:07 +0000

    🏬🛳 Checkpoint ./lib/definitions.js:27704817/31 ./server.js:27704817/6

--- server.js
+++ server.js
@@ -4943,7 +4943,7 @@ var maintainloop = (() => {
         function getFoodClass(level, isGreenShape) {
             let a = { };
             switch (level) {
-                case 0: a = Class.egg; break;
+                case 0: a = Class.gem; break;
                 case 1: a = Class.square; break; 
                 case 2: a = Class.triangle; break;
                 case 3: a = Class.pentagon; break;
