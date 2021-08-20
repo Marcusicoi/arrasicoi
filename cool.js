4c91a681c8fcd4d504cc9b786d5badcd3b4fdc27
Glitch (arrasicoi-client-f) <none>
2021-08-20 10:34:15 +0000

    😋🌸 Checkpoint ./cool.js:27704817/732

---
+++ cool.js
@@ -0,0 +1,8 @@
+if let instanceof in for this do
+
+//what happens if chat is dead for me?
+do (entity.o = chat.dead => {
+if (chat.dead = status.offline)
+  
+if (chat.active = status.online)
+else (chat.active) while status.coding = [status === status.donotdisturb]
