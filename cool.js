314cf0bcf4dbbb0f1b3edb26923b0ada1ba2f27d
Glitch (arrasicoi-client-f) <none>
2021-08-21 11:12:22 +0000

    👠💒 Checkpoint ./cool.js:27704817/226

--- cool.js
+++ cool.js
@@ -1,5 +1,5 @@
-
-/*what happens if chat is dead for me?
+do let
+//what happens if chat is dead for me?
 let app = (basic.apps) {
 if (gadget.PC = (category.games)) else (category.apps)
 let (gadget.android) else (gadget.ios) {gadget.PC}
@@ -7,10 +7,10 @@ let discord = (redirect.html(google.play.store) search("discord") number(1))
 let arras = (redirect.html("discord.gg/arras") <= app.discord)
 let channel = (app.discord => server.arras <= channel.channel)
 let chat = (app.discord => server.arras <= channel.general)
+let status = (profile.marcusicoi9150 = discord.settings.status)
 
 do (entity.o = chat.dead => {
 if (chat.dead = status.offline)
 else (chat.dead = turn.status ? status.coding)
 if (chat.active = status.online)
 else (chat.active) while status.coding = [status === status.donotdisturb]
-*/
