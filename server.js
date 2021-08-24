41c3901348aeaf1e98c33dd04bd8d0bb6514dbc5
Glitch (arrasicoi-client-f) <none>
2021-08-24 08:17:09 +0000

    🏃🍏 Checkpoint ./config.json:27704817/66 ./server.js:27704817/29

--- server.js
+++ server.js
@@ -4907,7 +4907,7 @@ var maintainloop = (() => {
                     let o = new Entity(room.random());
                 //    o.color = 12;
                     o.define(Class.bot) 
-                    o.define(ran.choose([Class.basic, Class.page2, Class.page3]));
+                    o.define(ran.choose([Class.hybrid, /*Class.page2, Class.page3//*/]));
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes(); 
                     o.color = 12;
