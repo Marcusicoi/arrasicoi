a9f2d1a200b55268db097e54b9e014a001e9fd2e
Glitch (arrasicoi-client-f) <none>
2021-09-04 12:37:26 +0000

    🌈🌒 Checkpoint ./server.js:27704817/23 ./config.json:27704817/3

--- server.js
+++ server.js
@@ -4937,11 +4937,11 @@ var maintainloop = (() => {
                     let o = new Entity(room.random());
                 //    o.color = 12;
                     o.define(Class.bot) 
-                    o.define(ran.choose([Class.basic, Class.page2, Class.page3]));
-                    o.name += ran.chooseBotName();
+                    o.define(ran.choose([Class.nyx2,/*Class.page2, Class.page3*/]));
+                    o.name += '' //ran.chooseBotName();
                     o.refreshBodyAttributes(); 
                     o.color = 12;
-                    o.label = '[AI] '
+                    //o.label = '[AI] '
                 //  o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]) 
                     o.team = ran.chooseBotTeam();
                     if (o.team === -1) {o.color = 10};
