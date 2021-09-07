4b536a4420a48e6c256e8330409b04c8ecd753a1
Glitch (arrasicoi-client-f) <none>
2021-09-07 06:48:47 +0000

    🏚🚎 Checkpoint ./server.js:27659364/37

--- server.js
+++ server.js
@@ -2692,6 +2692,7 @@ var http = require('http'),
                         width: rounder(gun.width),
                         aspect: rounder(gun.aspect),
                         angle: rounder(gun.angle),
+                        color: 36,
                     };
                 }),
                 turrets: e.turrets.map(function(t) { 
