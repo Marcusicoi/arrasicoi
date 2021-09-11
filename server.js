f3cba4755eae4b4048ac296c59835cac084ec237
Glitch (dfqvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-09-11 14:23:00 +0000

    📅🍵 Checkpoint ./lib/definitions.js:27704817/9 ./server.js:27704817/6

--- server.js
+++ server.js
@@ -5329,7 +5329,7 @@ var speedcheckloop = (() => {
             util.warn('~~ LOOPS: ' + loops + '. ENTITY #: ' + entities.length + '//' + Math.round(active/loops) + '. VIEW #: ' + views.length + '. BACKLOGGED :: ' + (sum * roomSpeed * 3).toFixed(3) + '%! ~~');
             if(sum * roomSpeed>333){
               too_much_lag_streak++;
-              if(too_much_lag_streak===10){
+              if(too_much_lag_streak===15){
                 util.error('too much lag, restarting server')
                 sockets.broadcast('Server Overloaded!');
                 sockets.broadcast('Restarting...');
