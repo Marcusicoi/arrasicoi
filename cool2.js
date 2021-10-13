71f6f8611673928e07626013597676454f44f03f
Glitch (dfqvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-10-13 10:31:44 +0000

    ⏳😄 Checkpoint ./cool2.js:27704817/935

--- cool2.js
+++ cool2.js
@@ -1,5 +1,6 @@
 //Require
 const main = require('server.js');
+const exports = require('definitions.js');
 //Entity
 var type = ['tank', 'food', 'boss', 'guns'];
 var tank = type[0];
@@ -15,19 +16,22 @@ const entity = {
 //Guns
 var gunsType = ['length', 'width', 'aspect', 'x', 'y', 'angle', 'delay'];
 var gun = [entity.barrel, gunsType[0,1,2,3,4,5,6]];
-var length = gun[gunsType[0]];
-var width = gun[gunsType[1]];
-var aspect = gun[gunsType[2]];
-var x = gun[gunsType[3]];
-var y = gun[gunsType[4]];
-var angle = gun[gunsType[5]];
-var delay = gun[gunsType[6]];
 const guns = {
-length: [length, main.LENGTH],
-width: [width, main.WIDTH],
-aspect: [aspect, main.ASPECT],
-x: [x, main.X],
-y: [y, main.Y],
-angle: [angle, main.ANGLE],
-delay: [delay, main.DELAY],
+length: [gun[gunsType[0]], main.LENGTH],
+width: [gun[gunsType[1]], main.WIDTH],
+aspect: [gun[gunsType[2]], main.ASPECT],
+x: [gun[gunsType[3]], main.X],
+y: [gun[gunsType[4]], main.Y],
+angle: [guns[gunsType[5]], main.ANGLE],
+delay: [guns[gunsType[6]], main.DELAY],
 }  
+//Foods
+var foodType = ['egg', 'square', 'triangle', 'pentagon']
+var food = [entity.food, foodType[0,1,2,3]];
+if (foodType['pentagon'] === food[foodType[3]]) ([
+  return: {
+   }])
+const foods = {
+egg: [food[foodType[0]], exports.egg],
+square: [food[foodType[1]], exports.square],
+triangle: [food[foodType[2]], exports.triangle],
