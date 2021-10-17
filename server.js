ecf3fe9a66e051f6f78283c2372c8902c5648654
Glitch (dfqvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-10-17 13:04:04 +0000

    Exporting to Marcusicoi69/arrasicoi ./server.js:27704817/163 ./lib/definitions.js:27704817/12

--- server.js
+++ server.js
@@ -5010,7 +5010,7 @@ var maintainloop = (() => {
     })(); 
    
     let spawnRareShapes = (() => {
-        const config = {
+        const option = {
             max: 1000,
             chance: 1, //0.00002
             legendChance: 1, //0.000001
@@ -5018,16 +5018,16 @@ var maintainloop = (() => {
             legendaries: [Class.jewel, Class.lsqu, Class.ltri, Class.lpenta, Class.lbpenta, Class.lhpenta],
         };
         return census => {
-            if (census.crasher < config.max) {
-                for (let i = 0; i < config.max - census.crasher; i++) {
-                    if (Math.random() > config.chance) {
+            if (census.crasher < option.max) {
+                for (let i = 0; i < option.max - census.crasher; i++) {
+                    if (Math.random() > option.chance) {
                         let spot, i = 30;
                         do {
-                            spot = room.randomType(room.random());
+                            spot = room.randomType('nest', 'norm');
                             i--;
                             if (!i) return 0;
-                        } while (dirtyCheck(spot, 100));
-                        const type = ran.choose(([config.shinies, config.legendaries][+(Math.random() > config.legendChance)]));
+                        } while (dirtyCheck(spot, 1000));
+                        const type = ran.choose(([option.shinies, option.legendaries][+(Math.random() > option.legendChance)]));
                         let o = new Entity(spot);
                         o.define(type);
                         o.team = -100;
@@ -5038,7 +5038,7 @@ var maintainloop = (() => {
     })();        
     let spawnCrasher = (() => {
         const config = {
-            max: 1, // The max amount of crashers/sentries
+            max: 10, // The max amount of crashers/sentries
             chance: 1, // Math.random() must be greater than this in order to spawn anything
             sentryChance: 1, // Math.random() must be greater than this for a sentry spawn.
             crashers: [Class.crasher, Class.autoCrash], // Crasher Types
@@ -5053,7 +5053,7 @@ var maintainloop = (() => {
                             spot = room.randomType('nest');
                             i --;
                             if (!i) return 0;
-                        } while (dirtyCheck(spot, 5000));
+                        } while (dirtyCheck(spot, 1000));
                         const type = ran.choose(([config.crashers, config.sentries][+(Math.random() > config.sentryChance)]));
                         let o = new Entity(spot);
                         o.define(type);
