8f17a44e25c7444d75b6a06b5ebef30eebc4e12e
Glitch (dfqvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-10-20 02:47:41 +0000

    🏑📉 Checkpoint ./server.js:27704817/1641

--- server.js
+++ server.js
@@ -5381,14 +5381,29 @@ var maintainloop = (() => {
                     } while (o.id === oldId && --overflow);        
                     if (!overflow) continue;
                     // Configure for the nest if needed
-                    let proportions = c.FOOD,
-                        cens = census,
-                        amount = foodAmount;
-                    if (room.isIn('nest', o)) {
-                        proportions = c.FOOD_NEST;
-                        cens = censusNest;
-                        amount = nestFoodAmount;
-                    }
+                    if (o.foodLevel.toString() in c.FOODPATHS) {
+                            let probabilities = c.FOODPATHS[o.foodLevel.toString()][0][0],
+                                cens = census,
+                                amount = foodAmount;
+                            if (room.isIn('nest', o)) {
+                                probabilities = c.FOODPATHS[o.foodLevel.toString()][1][0],
+                                    cens = censusNest;
+                                amount = nestFoodAmount;
+                            }
+                            // Upgrade stuff
+                            o.foodCountup += Math.ceil(Math.abs(ran.gauss(0, 10)));
+                            while (o.foodCountup >= (o.foodLevel + 1) * 100) {
+                                o.foodCountup -= (o.foodLevel + 1) * 100;
+                                if (ran.chance(1 - cens[o.foodLevel + 1] / amount / probabilities)) {
+                                    o.define(getFoodClass(o.foodLevel, true));
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    } 
+                  )()
                     // Upgrade stuff
                     o.foodCountup += Math.ceil(Math.abs(ran.gauss(0, 10)));
                     while (o.foodCountup >= (o.foodLevel + 1) * 100) {
@@ -5399,7 +5414,7 @@ var maintainloop = (() => {
                     }
                 }
             }
-        };
+        }
     })();
     // Define food and food spawning
     return () => {
