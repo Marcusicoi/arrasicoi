c0fa039fe35fd6d334108c49e47134315fb52b0e
Glitch (dfrvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-10-24 11:43:38 +0000

    😎🍔 Checkpoint ./config.json:27704817/32 ./server.js:27704817/8

--- server.js
+++ server.js
@@ -5226,9 +5226,9 @@ var maintainloop = (() => {
                     let o = new Entity(room.random());
                 //  o.color = 12;
                     o.define(Class.bot) 
-                 //o.define(ran.choose([Class.basic, Class.page2, Class.page3]));
+                   //o.define(ran.choose([Class.basic, Class.page2, Class.page3]));
                     o.define(Class.tyr2);
-                    o.name += ran.chooseBotName();
+                   // o.name += ran.chooseBotName();
                     o.refreshBodyAttributes(); 
                     o.color = 12;
                     o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]) 
