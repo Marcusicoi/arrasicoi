46c751a79f35fc4859e1cada9d82416f064ce0cf
Glitch (dfrvi2951-eib81eia-oiwo929uwu-developer-client-f) <none>
2021-10-31 05:20:49 +0000

    🛩🕰 Checkpoint ./server.js:27704817/861

--- server.js
+++ server.js
@@ -5161,11 +5161,9 @@ var maintainloop = (() => {
     })();
     let spawnRareShapes = (() => {
         const config = {
-            max: 10, // The max amount of rare shapes
+            max: 1000, // The max amount of shinyshapes
             shinyChance: 0.000000000005, // Math.random() must be greater than this for a shiny spawn.
-            legendChance: 0.000000000000001, // Math.random() must be greater than this for a legend spawn.
             shinies: [Class.gem, Class.gsqu, Class.gtri, Class.gpenta], // Shiny Types
-            legendaries: [Class.jewel, Class.lsqu, Class.ltri, Class.lpenta] // Legendary types
         };
         return census => {
             if (census.crasher < config.max) {
@@ -5181,19 +5179,7 @@ var maintainloop = (() => {
                         let o = new Entity(spot);
                         o.define(type);
                         o.team = -100;
-                    } else 
-                    if (Math.random() > config.legendChance) {
-                       let spot, i = 30;
-                       do {
-                           spot = room.randomType(ran.choose(['norm', 'nest', 'roid']));
-                           i --;
-                           if (!i) return 0;
-                        } while (dirtyCheck(spot, 500));
-                        const type = (config.legendaries);
-                        let o = new Entity(spot);
-                        o.define(type);
-                        o.team = -100;
-                    }
+                    } 
                 }
             }
         }
