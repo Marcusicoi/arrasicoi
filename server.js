a0385dd114bbb70a593320d06d94f017c01eb5b7
Glitch (arrasicoi-glitch-source-dev-2) <none>
2021-11-07 11:11:20 +0000

    🐵🕶 Checkpoint ./server.js:27704817/63

--- server.js
+++ server.js
@@ -5249,15 +5249,14 @@ var maintainloop = (() => {
                     let o = new Entity(room.random());
                     o.define(Class.bot) 
                     o.define(ran.choose([Class.basic, Class.page2, Class.page3]));
-                  //  o.define(Class.fiolnir2);
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes(); 
                     o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]) 
-                  //  o.team = ran.chooseBotTeam();
-                  /*  if (o.team === -1) {o.color = 10};
+                    o.team = ran.chooseBot2Team();
+                    if (o.team === -1) {o.color = 10};
                     if (o.team === -2) {o.color = 11};
                     if (o.team === -3) {o.color = 12};
-                    if (o.team === -4) {o.color = 15};*/
+                    if (o.team === -4) {o.color = 15};
                     bots.push(o);
                 }
                   // Remove dead ones
