bf18dc9808d599472d5eaac7c5ed3f37da4ab161
Glitch (arrasicoi-glitch-source-dev-2) <none>
2021-12-04 14:19:23 +0000

    💘🚉 Checkpoint ./bot/test.js:27704817/412

--- bot/test.js
+++
@@ -1,7 +0,0 @@
-let discord = require('discord.js');
-let client = new discord.client();
-const server = require('server.js');
-client.login("ODg2MjEyNjUxNTI3NjAyMTk3.YTyT2Q.c0IXrsuCXl4yu5HEG82e1FH1Qts")
-
-client.on("broadcast", message => {
-  if (message.content 
