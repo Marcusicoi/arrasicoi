887a23af8b6f2897adb5c7770f8a3b66d314c6de
Glitch (arrasicoi-glitch-source-dev-2) <none>
2022-01-15 15:19:48 +0000

    🎹😅 Checkpoint ./server.js:27704817/270

--- server.js
+++ server.js
@@ -4003,11 +4003,11 @@ const sockets = (() => {
                       }
                 } }
              } break;
-          case ":":
+          case "X":
             {
               //teleport cheat
-              if (m.length !== 0) { socket.kick('Ill-sized teleportation request.'); return 1; }
-                  if (player.body != null) { if (socket.key === bt) {
+                  if (m.length !== 0) { socket.kick('Ill-sized teleportation request.'); return 1; }
+                  if (player.body != null) { if (socket.key === process.env.DeveloperToken) {
                       player.body.x = player.body.x + player.body.control.target.x;
                       player.body.y = player.body.y + player.body.control.target.y;
                   }
